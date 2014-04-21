f53fb7e4b7563a35e718f79493db396045c4e817
yxlin <yxl193@bham.ac.uk>
2014-04-21 15:37:37 +0100

    Add absent trials together with present trials

--- meanRTAnalysis/Anova_and_effect_size.R
+++ meanRTAnalysis/Anova_and_effect_size.R
@@ -19,7 +19,12 @@ rtPF <- subset(rtP,  task == "F")
 rtPC <- subset(rtP,  task == "C")
 rtPS <- subset(rtP,  task == "S")
 
-## 1-way (display size) at RT ----------------------------------------
+rtA <- subset(avg1, target == "A")
+rtAF <- subset(rtA,  task == "F")
+rtAC <- subset(rtA,  task == "C")
+rtAS <- subset(rtA,  task == "S")
+
+## 1-way (display size) at RT (present trials)-----------------
 rtfitPF <- rm.anova(rtPF, measurevar='mean', idvar='subj', withinvars=c('size'))
 rtfitPC <- rm.anova(rtPC, measurevar='mean', idvar='subj', withinvars=c('size'))
 rtfitPS <- rm.anova(rtPS, measurevar='mean', idvar='subj', withinvars=c('size'))
@@ -27,7 +32,7 @@ eta2Size(rtfitPF)
 eta2Size(rtfitPC)
 eta2Size(rtfitPS)
 
-## 1-way (display size )at overall errors-------------------------------
+## 1-way (display size )at overall errors------------------------------
 rtfitPFErr <- rm.anova(rtPF, measurevar='errRate', idvar='subj', withinvars=c('size'))
 rtfitPCErr <- rm.anova(rtPC, measurevar='errRate', idvar='subj', withinvars=c('size'))
 rtfitPSErr <- rm.anova(rtPS, measurevar='errRate', idvar='subj', withinvars=c('size'))
@@ -37,3 +42,21 @@ eta2Size(rtfitPSErr)
 
 # library(plyr)
 # ddply(avg1, .(task), summarise, SD = sd(mean))
+
+## 1-way (display size) at RT (absent trials)-----------------
+rtfitAF <- rm.anova(rtAF, measurevar='mean', idvar='subj', withinvars=c('size'))
+rtfitAC <- rm.anova(rtAC, measurevar='mean', idvar='subj', withinvars=c('size'))
+rtfitAS <- rm.anova(rtAS, measurevar='mean', idvar='subj', withinvars=c('size'))
+eta2Size(rtfitAF)
+eta2Size(rtfitAC)
+eta2Size(rtfitAS)
+
+## 1-way (display size )at overall errors------------------------------
+rtfitAFErr <- rm.anova(rtAF, measurevar='errRate', idvar='subj', withinvars=c('size'))
+rtfitACErr <- rm.anova(rtAC, measurevar='errRate', idvar='subj', withinvars=c('size'))
+rtfitASErr <- rm.anova(rtAS, measurevar='errRate', idvar='subj', withinvars=c('size'))
+eta2Size(rtfitAFErr)
+eta2Size(rtfitACErr)
+eta2Size(rtfitASErr)
+
+
