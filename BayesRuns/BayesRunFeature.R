470d3f8945d42f68aa6f145bb07b109eeedbcc79
yxlin <yxl193@bham.ac.uk>
2014-06-11 10:42:23 +0100

    organise the code to be able to view in one screen.

--- BayesRuns/BayesRunFeature.R
+++ BayesRuns/BayesRunFeature.R
@@ -1,21 +1,21 @@
 rm(list=ls())
-# Disclaimer-------------------------------------------------------
+# Disclaimer------------------------------------------------------
 # Author: Yishin Lin
 # Date: 21 May, 2013
 # Description: Run HBM
 
-# Load data and functions -----------------------------------------
+# Load data and functions--------------------------------------
 load('./data/myData/featureT.RData')
 # Gelman and Hill's (2007) random imputation function
 source("./functions/random.imp.R")
 
-# Set up sequences of each factor  --------------------------------
+# Set up sequences of each factor------------------------------
 size.seq <- sort(unique(featureT$size))
 target.seq <- sort(unique(featureT$target))
 # We should have 19 (out of 20 participants) valid participants 
 NSubj <- length(unique(featureT$subj))
 
-# Start the for loop------------------------------------------------
+# Start the for loop-------------------------------------------
 for(i in seq(along = size.seq)){
   subdata1 <- subset(featureT, size == size.seq[i])
   if (nrow(subdata1)  == 0) next
@@ -75,15 +75,15 @@ for(i in seq(along = size.seq)){
     
     # Set initial prior values------------------------------
     # for each participant, set prior beta=0.9;  lambda=12, 
-    # theta (scale) = lambda^(-1/beta); psi = min(rt.sec) - .2
-
+    # theta (scale) = lambda^(-1/beta); 
+    # psi = min(each particpant) - min in the group
 	  thetaInit <-  runif(NSubj, .3, 4)  
 	  betaInit <- runif(NSubj, 0.9, 2)
 	  psiInit <- dataPerSubj.ordered$Min -  
       min(dataPerSubj.ordered$Min)
 
 
-# Initialization ---------------------------------------------------
+# Initialization ----------------------------------------------
 # Rouder et al.'s (2005) initial values
 # "to simulate analysis of real data, these should be 
 # reasonable, but far from true values"
@@ -96,7 +96,7 @@ for(i in seq(along = size.seq)){
         xi1 = .1, xi2 = .1)
   }
 
-# Define parameters to monitor -------------------------------------
+# Define parameters to monitor ----------------------------------
 parameters <- c('beta', 'psi', 'theta')
 nb <-  5000; nbt <-  10
 nc <- 3; nct <- 1
@@ -104,10 +104,14 @@ ni <-  105000; nit <-  1000
 nt <- 4; ntt <- 1
 
 # RUN THE CHAINS (Create, initialize, and adapt the model:)
-# Test run ---------------------------------------------------------
-# jagsfitT <- jags(data=JAGSName, model.file='./BayesRuns/model.txt', inits=initList, parameters.to.save=parameters, n.iter=nit, n.chain = nct, n.burnin = nbt, n.thin = ntt)
+# Test run ------------------------------------------------------
+# jagsfitT <- jags(data=JAGSName, 
+# model.file='./BayesRuns/model.txt', 
+# inits=initList, parameters.to.save=parameters, 
+# n.iter=nit, n.chain = nct, n.burnin = nbt, 
+# n.thin = ntt)
 
-# Real run ---------------------------------------------------------
+# Real run ------------------------------------------------------
 jagsfit <- jags(data=JAGSName, 
                 model.file='./BayesRuns/model.txt', 
                 inits=initList, parameters.to.save=parameters, 
