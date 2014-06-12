cd5db438036978dcb484f25b657796d447519ca5
yxlin <yxl193@bham.ac.uk>
2014-06-12 14:50:56 +0100

    tidy up the codes

--- distributionAnalysis/trialLevelDensity/densityPlot.R
+++ distributionAnalysis/trialLevelDensity/densityPlot.R
@@ -20,11 +20,13 @@ den <- ggplot(ylinrt, aes(x=rt, colour=as.numeric(subjNum),
                        group=as.numeric(subjNum)))
 den <- den + geom_density(fill=NA, size=.5) +
     facet_grid(size~taskTgt, scales="free") + theme_bw() + 
-    scale_x_continuous(breaks=seq(0, 10000, 1500), name="RT (ms)") + 
+    scale_x_continuous(breaks=seq(0, 10000, 1500), 
+                       name="RT (ms)") + 
     scale_y_continuous(breaks=seq(0, 1, .004), name ="Density") +
     coord_cartesian(xlim = c(0, 4300)) +
     theme(axis.title.x = element_text(size=34), 
-          axis.text.x  = element_text(angle=70, size=30, vjust = 1, hjust = 1),   
+          axis.text.x  = element_text(angle=70, size=30, 
+                                      vjust = 1, hjust = 1),   
           axis.title.y = element_text(angle=90, size=30),
           axis.text.y  = element_text(size=34),
           strip.text.x = element_text(size=30, angle=0),
